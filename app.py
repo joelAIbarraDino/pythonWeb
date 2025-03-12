7464946a47a80bd36868fac3b8b3e9d9ad36d060
joelAIbarraDino <joel.ibarra@dinozign.com>
2025-03-12 17:52:15 -0600

    configuracion de dataset en aplicacion

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('dataset_prueba.csv')
 
 st.header('Analisis de datos de vehiculos en estados unidos')
 
